c779f6e60fa68daf407907e28cfb91971adfb375
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-08 14:55:46 +0100

    Birthday class and rspec tests

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,3 +1,22 @@
+require 'date'
+
 class Birthday
+  DATE_FORMAT = '%d-%m-%Y'
+
+  attr_accessor :birthday_date
+
+  def initialize(birthday_date)
+    @birthday_date = birthday_date
+  end
+
+  def count
+    bday = Date.parse(birthday_date)
+    this_year = Date.new(Date.today.year, bday.month, bday.day)
+    if this_year > Date.today 
+      (this_year - Date.today).to_i
+    else
+      (Date.new(Date.today.year + 1, bday.month, bday.day) - Date.today).to_i
+    end
+  end
 
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,9 +1,24 @@
-require "birthday"
+require 'birthday'
+require 'timecop'
 
 describe Birthday do
+  subject(:birthday) { Birthday.new(birthday_date) }
+  let(:birthday_date) { '10/12/1986' }
 
-  describe '#' do
+  describe '#birthday date' do
+    it 'sets a birthday date' do
+      expect(birthday.birthday_date).to eq '10/12/1986'
+    end
 
   end
   
+  describe '#count' do
+    it 'shows how many days until your birthday' do
+      Timecop.freeze(Time.parse('08/05/2022')) do
+        allow(birthday).to receive(:birthday_date).and_return('10/12/1986')
+        expect(birthday.count).to eq 216
+      end
+    end
+  end
+
 end
